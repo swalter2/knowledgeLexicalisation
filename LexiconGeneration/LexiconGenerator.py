f71ff48952224e7f72795866b8204854552bd386
Sebastian Walter <swalter@techfak.uni-bielefeld.de>
2013-08-16 15:29:42 +0200

    started using lemon.patterns

--- LexiconGeneration/LexiconGenerator.py
+++ LexiconGeneration/LexiconGenerator.py
@@ -123,13 +123,15 @@ def createClassEntry(uri,en_target_lexicon):
     for entry in array:
         for x in StandardLexiconEntries.createLabel(entry,en_target_lexicon):
             x = x.replace("_"," ")
-            lexEntry = ":"+x.replace(" ","").lower()+" a lemon:LexicalEntry ;\n"
-            lexEntry += "lexinfo:partOfSpeech lexinfo:noun ;\n"
-            lexEntry += "lemon:canonicalForm [ lemon:writtenRep \""+x.lower()+"\"@en ];\n"
-            lexEntry += "lemon:synBehavior [ rdf:type lexinfo:NounPredicateFrame ;\n"
-            lexEntry += "lexinfo:subject :arg ];\n"
-            lexEntry += "lemon:sense [ lemon:reference <"+uri+"> ;\n"
-            lexEntry += "lemon:isA :arg ] ." 
+
+            lexEntry = "ClassNoun(\""+ x+"\",<"+uri+">)"
+#             lexEntry = ":"+x.replace(" ","").lower()+" a lemon:LexicalEntry ;\n"
+#             lexEntry += "lexinfo:partOfSpeech lexinfo:noun ;\n"
+#             lexEntry += "lemon:canonicalForm [ lemon:writtenRep \""+x.lower()+"\"@en ];\n"
+#             lexEntry += "lemon:synBehavior [ rdf:type lexinfo:NounPredicateFrame ;\n"
+#             lexEntry += "lexinfo:subject :arg ];\n"
+#             lexEntry += "lemon:sense [ lemon:reference <"+uri+"> ;\n"
+#             lexEntry += "lemon:isA :arg ] ." 
             result_array.append(lexEntry)
     return result_array
 
@@ -141,21 +143,22 @@ def NounPossisiveFrame(term,reference):
     Creates an NounPossisiveFrame  entry for a given label and reference with a standard "of" marker
     """
     term = term.replace(";","")
-    entry = ":"+term.lower().replace(" ","_")+" a lemon:LexicalEntry ;\n"
-    entry += "lemon:canonicalForm [ lemon:writtenRep \""+term+"\"@en ];\n" 
-    entry += "lemon:sense [ lemon:reference <"+reference+"> ;\n"
-    entry += "lemon:subjOfProp :arg1 ;\n"
-    entry += "lemon:objOfProp  :arg2 ] ;\n"
-    entry += "lemon:synBehavior [ rdf:type lexinfo:NounPossessiveFrame ;\n"
-    entry += "lexinfo:copulativeArg :arg2 ;\n"  
-    entry += "lexinfo:possessiveAdjunct :arg1 ] ;\n"
-    entry += "lexinfo:partOfSpeech lexinfo:noun .\n"
-    if language == "German":
-        entry += ":y_arg2 lemon:marker [ lemon:canonicalForm [ lemon:writtenRep \"von\"] ]."
-    elif language == "English":
-        entry += ":y_arg2 lemon:marker [ lemon:canonicalForm [ lemon:writtenRep \"of\"] ]."
-    else:
-        entry += ":y_arg2 lemon:marker [ lemon:canonicalForm [ lemon:writtenRep \"of\"] ]."
+    entry = "RelationalNoun(\""+term+"\",<"+reference+">, propSubj = PossessiveAdjunct, propObj  = CopulativeArg)"
+#     entry = ":"+term.lower().replace(" ","_")+" a lemon:LexicalEntry ;\n"
+#     entry += "lemon:canonicalForm [ lemon:writtenRep \""+term+"\"@en ];\n" 
+#     entry += "lemon:sense [ lemon:reference <"+reference+"> ;\n"
+#     entry += "lemon:subjOfProp :arg1 ;\n"
+#     entry += "lemon:objOfProp  :arg2 ] ;\n"
+#     entry += "lemon:synBehavior [ rdf:type lexinfo:NounPossessiveFrame ;\n"
+#     entry += "lexinfo:copulativeArg :arg2 ;\n"  
+#     entry += "lexinfo:possessiveAdjunct :arg1 ] ;\n"
+#     entry += "lexinfo:partOfSpeech lexinfo:noun .\n"
+#     if language == "German":
+#         entry += ":y_arg2 lemon:marker [ lemon:canonicalForm [ lemon:writtenRep \"von\"] ]."
+#     elif language == "English":
+#         entry += ":y_arg2 lemon:marker [ lemon:canonicalForm [ lemon:writtenRep \"of\"] ]."
+#     else:
+#         entry += ":y_arg2 lemon:marker [ lemon:canonicalForm [ lemon:writtenRep \"of\"] ]."
     return entry
 
 def NounPossisiveFrameWithoutMarker(term,reference):
@@ -163,36 +166,44 @@ def NounPossisiveFrameWithoutMarker(term,reference):
     Creates an NounPossisiveFrame  entry for a given label and reference, but without any marker
     """
     term = term.replace(";","")
-    entry = ":"+term.lower().replace(" ","_")+" a lemon:LexicalEntry ;\n"
-    entry += "lemon:canonicalForm [ lemon:writtenRep \""+term+"\"@en ];\n" 
-    entry += "lemon:sense [ lemon:reference <"+reference+"> ;\n"
-    entry += "lemon:subjOfProp :arg1 ;\n"
-    entry += "lemon:objOfProp  :arg2 ] ;\n"
-    entry += "lemon:synBehavior [ rdf:type lexinfo:NounPossessiveFrame ;\n"
-    entry += "lexinfo:copulativeArg :arg2;\n"
-    entry += "lexinfo:possessiveAdjunct :arg1 ] ;\n"
-    entry += "lexinfo:partOfSpeech lexinfo:noun ."
+    entry = "RelationalNoun(\""+term+"\",<"+reference+">, propSubj = PossessiveAdjunct, propObj  = CopulativeArg)"
+     
+#     entry = ":"+term.lower().replace(" ","_")+" a lemon:LexicalEntry ;\n"
+#     entry += "lemon:canonicalForm [ lemon:writtenRep \""+term+"\"@en ];\n" 
+#     entry += "lemon:sense [ lemon:reference <"+reference+"> ;\n"
+#     entry += "lemon:subjOfProp :arg1 ;\n"
+#     entry += "lemon:objOfProp  :arg2 ] ;\n"
+#     entry += "lemon:synBehavior [ rdf:type lexinfo:NounPossessiveFrame ;\n"
+#     entry += "lexinfo:copulativeArg :arg2;\n"
+#     entry += "lexinfo:possessiveAdjunct :arg1 ] ;\n"
+#     entry += "lexinfo:partOfSpeech lexinfo:noun ."
     return entry
   
 def TransitiveFrame(term, reference,marker):
     """
     Creates an TransitiveFrame entry for a given label, reference and marker
     """
+
     term = term.replace(";","")
-    entry = ":"+term.lower().replace(" ","_")+" a lemon:LexicalEntry ;\n"
-    entry += "lemon:canonicalForm [ lemon:writtenRep \""+term+"\"@en ] ;\n"
-    entry += "lemon:synBehavior [ rdf:type lexinfo:TransitiveFrame ;\n"
-    entry += "lexinfo:subject      :arg1 ;\n"
-    entry += " lexinfo:directObject :arg2 ] ;\n"
-    entry += "lemon:sense [ lemon:reference <"+reference+">;\n"
-    entry += "lemon:subjOfProp :arg1;\n"
-    entry += "lemon:objOfProp  :arg2 ] ;\n"
     if len(marker) == 0:
-        entry += "lexinfo:partOfSpeech lexinfo:verb ."
+        entry = "StateVerb(\""+term+"\",<"+reference+">, propSubj = DirectObject, propObj  = Subject)"
+        return entry
     else:
-        entry += "lexinfo:partOfSpeech lexinfo:verb .\n"
-        for x in marker:
-            entry +=":y_arg2 lemon:marker [ lemon:canonicalForm [ lemon:writtenRep \""+x+"\"] ].\n"
+        entry = "StateVerb(\""+term+"\",<"+reference+">,propObj  = PrepositionalObject(\""+marker[0]+"\"))"
+#     entry = ":"+term.lower().replace(" ","_")+" a lemon:LexicalEntry ;\n"
+#     entry += "lemon:canonicalForm [ lemon:writtenRep \""+term+"\"@en ] ;\n"
+#     entry += "lemon:synBehavior [ rdf:type lexinfo:TransitiveFrame ;\n"
+#     entry += "lexinfo:subject      :arg1 ;\n"
+#     entry += " lexinfo:directObject :arg2 ] ;\n"
+#     entry += "lemon:sense [ lemon:reference <"+reference+">;\n"
+#     entry += "lemon:subjOfProp :arg1;\n"
+#     entry += "lemon:objOfProp  :arg2 ] ;\n"
+#     if len(marker) == 0:
+#         entry += "lexinfo:partOfSpeech lexinfo:verb ."
+#     else:
+#         entry += "lexinfo:partOfSpeech lexinfo:verb .\n"
+#         for x in marker:
+#             entry +=":y_arg2 lemon:marker [ lemon:canonicalForm [ lemon:writtenRep \""+x+"\"] ].\n"
     return entry
 
 
@@ -200,21 +211,33 @@ def AdjectivePredicateFrame(term, reference, marker):
     """
     Creates an AdjectivePredicateFrame entry for a given label, reference and marker
     """
+    
     term = term.replace(";","")
-    entry = ":"+term.lower().replace(" ","_")+" a lemon:LexicalEntry ;\n"
-    entry += "lemon:canonicalForm [ lemon:writtenRep \""+term+"\"@en ] ;\n"
-    entry += "lemon:synBehavior [ rdf:type lexinfo:AdjectivePredicateFrame ;\n"
-    entry += "lexinfo:copulativeSubject :arg1;\n"
-    entry += "lexinfo:attributiveArg :arg1 ];\n"
-    entry += "lemon:sense [ lemon:reference <"+reference+">;\n"
-    entry += "lemon:subjOfProp :arg1;\n"
-    entry += "lemon:objOfProp  :arg2 ] ;\n"
-    if len(marker) == 0:
-        entry += "lexinfo:partOfSpeech lexinfo:adjective ."
-    else:
-        entry += "lexinfo:partOfSpeech lexinfo:adjective .\n"
-        for x in marker:
-            entry +=":arg2 lemon:marker [ lemon:canonicalForm [ lemon:writtenRep \""+x+"\"] ].\n"
+    entry = "RelationalAdjective(\""+term+"\",<"+reference+">, relationalArg = PrepositionalObject(\"to\"))"
+    return entry
+#     if len(marker) == 0:
+#         entry = "RelationalAdjective(\""+term+"\",<"+reference+">)"
+#         return entry
+#     else:
+#         entry = "RelationalAdjective(\""+term+"\",<"+reference+">, relationalArg = PrepositionalObject(\"to\"))"
+#         return entry
+    
+    
+#     term = term.replace(";","")
+#     entry = ":"+term.lower().replace(" ","_")+" a lemon:LexicalEntry ;\n"
+#     entry += "lemon:canonicalForm [ lemon:writtenRep \""+term+"\"@en ] ;\n"
+#     entry += "lemon:synBehavior [ rdf:type lexinfo:AdjectivePredicateFrame ;\n"
+#     entry += "lexinfo:copulativeSubject :arg1;\n"
+#     entry += "lexinfo:attributiveArg :arg1 ];\n"
+#     entry += "lemon:sense [ lemon:reference <"+reference+">;\n"
+#     entry += "lemon:subjOfProp :arg1;\n"
+#     entry += "lemon:objOfProp  :arg2 ] ;\n"
+#     if len(marker) == 0:
+#         entry += "lexinfo:partOfSpeech lexinfo:adjective ."
+#     else:
+#         entry += "lexinfo:partOfSpeech lexinfo:adjective .\n"
+#         for x in marker:
+#             entry +=":arg2 lemon:marker [ lemon:canonicalForm [ lemon:writtenRep \""+x+"\"] ].\n"
     return entry
 
 def AdjectivePPFrame(term, reference, marker):
@@ -222,18 +245,29 @@ def AdjectivePPFrame(term, reference, marker):
     Creates an AdjectivePPFrame entry for a given label, reference and marker
     """
     term = term.replace(";","")
-    entry = ":"+term.lower().replace(" ","_")+" a lemon:LexicalEntry ;\n"
-    entry += "lemon:canonicalForm [ lemon:writtenRep \""+term+"\"@en ] ;\n"
-    entry += "lemon:synBehavior [ rdf:type lexinfo:AdjectivePPFrame ;\n"
-    entry += "lexinfo:copulativeSubject   :arg1;\n"
-    entry += "lexinfo:prepositionalObject :arg2 ];\n"
-    entry += "lemon:sense [ lemon:reference <"+reference+"> ] ;\n"
-    if len(marker) == 0:
-        entry += "lexinfo:partOfSpeech lexinfo:adjective ."
-    else:
-        entry += "lexinfo:partOfSpeech lexinfo:adjective .\n"
-        for x in marker:
-            entry +=":arg2 lemon:marker [ lemon:canonicalForm [ lemon:writtenRep \""+x+"\"] ].\n"
+    entry = "RelationalAdjective(\""+term+"\",<"+reference+">, relationalArg = PrepositionalObject(\"to\"))"
+    return entry
+
+#     if len(marker) == 0:
+#         entry = "RelationalAdjective(\""+term+"\",<"+reference+">)"
+#         return entry
+#     else:
+#         entry = "RelationalAdjective(\""+term+"\",<"+reference+">, relationalArg = PrepositionalObject(\"to\"))"
+#         return entry
+
+
+#     entry = ":"+term.lower().replace(" ","_")+" a lemon:LexicalEntry ;\n"
+#     entry += "lemon:canonicalForm [ lemon:writtenRep \""+term+"\"@en ] ;\n"
+#     entry += "lemon:synBehavior [ rdf:type lexinfo:AdjectivePPFrame ;\n"
+#     entry += "lexinfo:copulativeSubject   :arg1;\n"
+#     entry += "lexinfo:prepositionalObject :arg2 ];\n"
+#     entry += "lemon:sense [ lemon:reference <"+reference+"> ] ;\n"
+#     if len(marker) == 0:
+#         entry += "lexinfo:partOfSpeech lexinfo:adjective ."
+#     else:
+#         entry += "lexinfo:partOfSpeech lexinfo:adjective .\n"
+#         for x in marker:
+#             entry +=":arg2 lemon:marker [ lemon:canonicalForm [ lemon:writtenRep \""+x+"\"] ].\n"
     return entry
 
 
@@ -242,18 +276,28 @@ def NounPPFrame(term,reference,marker):
     Creates an NounPPFrame entry for a given label, reference and marker
     """
     term = term.replace(";","")
-    entry = ":"+term.lower().replace(" ","_")+" a lemon:LexicalEntry ;\n"
-    entry += "lemon:canonicalForm [ lemon:writtenRep \""+term+"\"@en ] ;\n"
-    entry += "lemon:sense [ lemon:reference <"+reference+"> ;\n"
-    entry += "lemon:subjOfProp :arg1 ;\n"
-    entry += "lemon:objOfProp  :arg2 ] ;\n"
-    entry += "lemon:synBehavior [ rdf:type lexinfo:NounPPFrame ;\n"
-    entry += "lexinfo:copulativeArg :arg1 ;\n"
-    entry += "lexinfo:prepositionalAdjunct :arg2] ;\n"
+    entry = ""
+    #beim generieren der Pattern wird entschieden, was fuer ein frame gegeben ist, abhaengig dvon, ob eine praeposition gegeben ist oder nicht
     if len(marker) == 0:
-        entry += "lexinfo:partOfSpeech lexinfo:noun ."
+        entry = "RelationalNoun(\""+term+"\",<"+reference+">, propSubj = PossessiveAdjunct, propObj  = CopulativeArg)"
+        return entry
     else:
-        entry += "lexinfo:partOfSpeech lexinfo:noun .\n"
-        for x in marker:
-            entry +=":y_arg2 lemon:marker [ lemon:canonicalForm [ lemon:writtenRep \""+x+"\"] ].\n"
+        entry = "RelationalNoun(\""+term+"\",<"+reference+">, propSubj = PrepositionalObject(\""+marker[0]+"\"), propObj  = CopulativeArg)"
+        return entry
+     
+    
+#     entry = ":"+term.lower().replace(" ","_")+" a lemon:LexicalEntry ;\n"
+#     entry += "lemon:canonicalForm [ lemon:writtenRep \""+term+"\"@en ] ;\n"
+#     entry += "lemon:sense [ lemon:reference <"+reference+"> ;\n"
+#     entry += "lemon:subjOfProp :arg1 ;\n"
+#     entry += "lemon:objOfProp  :arg2 ] ;\n"
+#     entry += "lemon:synBehavior [ rdf:type lexinfo:NounPPFrame ;\n"
+#     entry += "lexinfo:copulativeArg :arg1 ;\n"
+#     entry += "lexinfo:prepositionalAdjunct :arg2] ;\n"
+#     if len(marker) == 0:
+#         entry += "lexinfo:partOfSpeech lexinfo:noun ."
+#     else:
+#         entry += "lexinfo:partOfSpeech lexinfo:noun .\n"
+#         for x in marker:
+#             entry +=":y_arg2 lemon:marker [ lemon:canonicalForm [ lemon:writtenRep \""+x+"\"] ].\n"
     return entry
